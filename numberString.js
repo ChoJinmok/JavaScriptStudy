649b139835ed40c2c17e49f325df512c4b13c120
ChoJinmok <jjmok1127@gmail.com>
2021-12-30 23:48:13 +0900

    Update numberString.js

--- numberString.js
+++ numberString.js
@@ -2,3 +2,15 @@ console.log(1);
 console.log(1.1+1.1); //JS는 실수 정수 구분하지 않고 numver로 통합해서 사용
 console.log(2*8);
 console.log(8/2);
+
+// 프로그래밍(컴퓨터)이란 맨처음 계산기로 고안 발전하다보니 지금의 컴퓨터가 됐음
+// 그래서 프로그래밍은 여러가지 계싼 기능을 포함하고 있음
+
+Math.pow(3,2);
+Math.round(10.6); //반올림
+Math.ceil(10.2); //올림
+Math.floor(10.2); //버림
+Math.sqrt(9); //제곱근
+Math.random(); //0~1 에서 랜덤한 실수
+var num1 = Math.round(100 * Math.random());
+console.log(num1);
