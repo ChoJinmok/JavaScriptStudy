b7742e721f17c216a7c568b0a9399cdedc262244
ChoJinmok <jjmok1127@gmail.com>
2021-12-31 23:54:26 +0900

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,17 @@
+// 변수 : 값을 담는 그릇(컨테이너) 언어에서 대명사와 비슷
+var a = 1;
+console.log(a+1);
+
+var b = 2;
+console.log(a+b);
+
+a = 2;
+console.log(a+b);
+
+var first = 'coding';
+console.log(first + 'everybody');
+first = '코딩';
+console.log(first + 'everybody');
+
+var a = 'coding', b = 'everybody';
+console.log(a+b);
