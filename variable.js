b68ee802a1d1c8b6780d10e6643538b8b32f9c34
ChoJinmok <jjmok1127@gmail.com>
2021-12-31 23:59:52 +0900

    Update variable.js

--- variable.js
+++ variable.js
@@ -15,3 +15,22 @@ console.log(first + 'everybody');
 
 var a = 'coding', b = 'everybody';
 console.log(a+b);
+
+
+// 코드는 왜 사용하는가?
+// 1. 코드의 재사용성이 증가한다.
+//    코드에서 변할 수 있는 영역과 변할 수 없는 영역 나눠서 보는게 좋다!
+//    변하지 않아야 하는 영역에 변할 수 있는 영역이 섞여 있으면 유지보수를 하기 어렵다.
+
+console.log(100+10);
+console.log((100+10)/10);
+console.log(((100+10)/10)-10);
+console.log((((100+10)/10)-10)*10);
+
+a = 100;
+a = a + 10;
+console.log(a);
+a = a / 10 ;
+console.log(a);
+a = a - 10;
+console
