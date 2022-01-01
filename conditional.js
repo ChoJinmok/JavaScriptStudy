458007ab799c7eee1585d09caa29f9eb066ab2e1
ChoJinmok <jjmok1127@gmail.com>
2022-01-01 15:06:28 +0900

    Create conditional.js

---
+++ conditional.js
@@ -0,0 +1,59 @@
+// 조건문은 컴퓨터와 계산기를 구분짓는 중요한 요소
+
+if (true) {
+    console.log('result : true');
+}
+
+if (false) {
+    console.log('result: true');
+}
+
+if (true) {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
+console.log(5);
+
+if (false) {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
+console.log(5);
+
+
+if (true) {
+    console.log(1);
+} else {
+    console.log(2);
+}
+
+if (false) {
+    console.log(1);
+} else {
+    console.log(2);
+}
+
+
+if (false) {
+    console.log(1);
+} else if (true) {
+    console.log(2);
+} else if (true) {
+    console.log(3);
+} else {
+    console.log(4);
+}
+
+if (false) {
+    console.log(1);
+} else if (false) {
+    console.log(2);
+} else if (true) {
+    console.log(3);
+} else {
+    console.log(4);
+}
