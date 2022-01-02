ed7b90c97df6b5e7a65feb8f5f1a1138b0e6475e
ChoJinmok <jjmok1127@gmail.com>
2022-01-02 23:09:19 +0900

    Create iterator.js

---
+++ iterator.js
@@ -0,0 +1,9 @@
+// while (true) { // 무한 반복함 -> 무한루프 컴퓨터에게 부담을 준다. -> 코딩하다보면 자주나올 수 있는 오류 -> 적당할때 false로 바껴야함
+//     console.log("Coding everybody");
+// }
+
+var i = 0;
+while (i < 10) {
+    console.log("Coding everybody");
+    i++;
+}
