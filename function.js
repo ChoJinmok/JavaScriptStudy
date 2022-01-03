06e025aad9f1be3e7626464b5afdc92a25fd2da7
ChoJinmok <jjmok1127@gmail.com>
2022-01-03 23:11:40 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-// 함수는 재용성! 을 높여줌, 재사용성을 익히는 것이 진짜 중요
+// 함수는 재용성! 을 높여줌, 재사용성(좋은 부품을 만드는것)을 익히는 것이 진짜 중요, 부품을 좋게 바꾸면 부품을 사용하는 곳도 다 개선됨 -> 유지보수 용의, 가독성 높아짐, 협업시에도 다른 사함이 만든 함수를 사용할때도 방법만 알면 사용하기 쉽다.
 
 
 //함수 정의
@@ -16,3 +16,31 @@ function numbering() {
     }
 }
 numbering()
+
+// 똑같은 기능을 여러 번 반복할때 함수의 효용이 드러난다.
+numbering()
+numbering()
+
+// 기계적으로 단순하게 반복될때는 반복문이 더 좋을 수 있음
+// 맥락에 맞게 반복해야한다면 함수!
+
+
+//return
+
+function get_member1() {
+    return 'egoing';
+}
+
+function  get_member2() {
+    return 'k8805';
+}
+
+function get_member3() {
+    return 'leezche'; // return을 처음만나면 종료됨 뒤에 코드 실행X
+    return 'graphittie';
+    return 'egoing';
+}
+
+console.log(get_member1());
+console.log(get_member2());
+console.log(get_member3());
