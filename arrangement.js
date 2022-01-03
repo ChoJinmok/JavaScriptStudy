b12154dae6a7f12349d209e1a04deebd2034dfa1
ChoJinmok <jjmok1127@gmail.com>
2022-01-03 23:55:36 +0900

    Create arrangement.js

---
+++ arrangement.js
@@ -0,0 +1,8 @@
+var members = ['egoing', 'k8805', 'sorialgi']; //정보를 처리해야될때 연관된 정보들을 묶어두면 편하게 사용 할 수 있다., 배열에 들어있는 값들을 원소(element)라고 한다.
+
+console.log(members);
+
+//원소번호는 0번 부터 시작, 번호는 색인(index)라고 부른다. 인덱스는 책 맨뒤에 색인, 정보를 찾아가는 번호
+console.log(members[0]);
+console.log(members[1]);
+console.log(members[2]);
