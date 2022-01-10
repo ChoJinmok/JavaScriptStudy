8045aaf66341a013cd05f237c489822e7b317caa
ChoJinmok <jjmok1127@gmail.com>
2022-01-10 23:58:36 +0900

    Update callback.js

--- callback.js
+++ callback.js
@@ -15,3 +15,26 @@ function decrease(num) {
 }
 console.log(cal(increase,1));
 console.log(cal(decrease,1));
+
+
+function cal2(mode) {
+    var funcs = {
+        'plus' : function(left, right) {return left + right},
+        'minus' : function(left, right) {return left - right}
+    }
+    return funcs[mode];
+}
+console.log(cal2('plus')(2,1));
+console.log(cal2('minus')(2,1));
+
+
+var process = [
+    function(input) {return input + 10;},
+    function(input) {return input * input;},
+    function(input) {return input / 2;}
+];
+var input = 1;
+for (var i = 0; i < process.length; i++) {
+    input = process[i](input);
+}
+console.log(input);
