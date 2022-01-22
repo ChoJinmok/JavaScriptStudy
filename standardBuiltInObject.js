db81ccd86e7bf9643fbf407d3f8fe9536a26a53a
ChoJinmok <jjmok1127@gmail.com>
2022-01-23 00:24:18 +0900

    Update standardBuiltInObject.js

--- standardBuiltInObject.js
+++ standardBuiltInObject.js
@@ -1,3 +1,4 @@
+/*
 var arr = new Array('seoul', 'new york', 'ladarkh', 'pusan', 'tsukuba');
 function getRandomValueFromArray(arr) {
     var index = Math.floor(arr.length * Math.random());
@@ -5,3 +6,12 @@ function getRandomValueFromArray(arr) {
     return result;
 }
 console.log(getRandomValueFromArray(arr));
+*/
+Array.prototype.random = function() {
+    var index = Math.floor(this.length * Math.random());
+    var result = this[index];
+    return result;
+}
+var arr = new Array('seoul', 'new york', 'ladarkh', 'pusan', 'tsukuba');
+console.log(arr.random());
+// Array객체에 메소드를 추가함으로서 가독성이 높아지고 메소드에 인자를 안넣어도 되게 해서 편의성도 높아짐
