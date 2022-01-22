604445d43650d3462e605dd002b47746f3ce636b
ChoJinmok <jjmok1127@gmail.com>
2022-01-23 00:16:36 +0900

    Update standardBuiltInObject.js

--- standardBuiltInObject.js
+++ standardBuiltInObject.js
@@ -1,5 +1,7 @@
 var arr = new Array('seoul', 'new york', 'ladarkh', 'pusan', 'tsukuba');
 function getRandomValueFromArray(arr) {
     var index = Math.floor(arr.length * Math.random());
-    return XPathResult;
+    var result = arr[index];
+    return result;
 }
+console.log(getRandomValueFromArray(arr));
