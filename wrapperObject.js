f411c7125afbf6a8fb25f8bfea6fffcf456eb322
ChoJinmok <jjmok1127@gmail.com>
2022-01-23 23:59:47 +0900

    Create wrapperObject.js

---
+++ wrapperObject.js
@@ -0,0 +1,11 @@
+var str = 'coding';
+console.log(str.length);
+// str.length를 한 순간
+// str new String('coding');이 일어남
+// 사용이 끝난 객체는 원시데이터타입으로 돌아감
+console.log(str.charAt(0));
+// str 다음에 오는 점 . 은 Object aceess operator이다. 이말은 str을 객채로 사용한 것
+
+str.prop = 'everyvody';
+// 다시 원시데이터로 돌아갔음
+console.log(str.prop);
